88c1edf7917f3f7658d353e6f2e90c6357704623
eunseo <songes04@gmail.com>
2026-03-05 20:15:04 +0100

    Update package-lock.json to upgrade Algolia dependencies to version 5.48.1 and remove unused components from app.ts.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { SearchInput } from '../components/search/search-input';
 import { ItemList } from '../components/selectable-items/item-list';
-import { ThreeValues } from '../components/three-values/three-values';
 
 @Component({
   selector: 'app-root',
-  imports: [SearchInput, ThreeValues, ItemList],
+  imports: [ItemList],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
