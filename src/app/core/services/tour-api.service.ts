4b2434b9a0555725af761e2e21a69245aacd8de0
eunseo <songes04@gmail.com>
2026-05-14 22:03:16 +0200

    refactor: clean up unused code

--- src/app/core/services/tour-api.service.ts
+++ src/app/core/services/tour-api.service.ts
@@ -1,4 +1,3 @@
-//new
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
--- src/app/features/tours/tours-page/tours-page.ts
+++ src/app/features/tours/tours-page/tours-page.ts
@@ -1,17 +1,3 @@
-/*import { Component } from '@angular/core';
-import { TourDetail } from '../tour-detail/tour-detail';
-import { TourSidebar } from '../tour-sidebar/tour-sidebar';
-
-@Component({
-  selector: 'app-tours-page',
-  standalone: true,
-  imports: [TourSidebar, TourDetail],
-  host: { class: 'flex min-h-0 flex-1 flex-col' },
-  templateUrl: './tours-page.html',
-})
-export class ToursPage {}*/
-
-//new
 import { Component, OnInit, inject } from '@angular/core';
 import { TourDetail } from '../tour-detail/tour-detail';
 import { TourSidebar } from '../tour-sidebar/tour-sidebar';
